b198eafee3f1c8e0eff6c5c6dbd498b7d674ce9a
AMIR <jramir669@gmail.com>
2021-07-19 16:20:26 +0430

    fix axios url

--- src/pages/axios.tsx
+++ src/pages/axios.tsx
@@ -7,12 +7,14 @@ interface Props {
 }
 
 export default function AxiosTest(props: Props) {
+  console.log(props);
+
   // TODO: once you get data map through data and show them name
   return <div>Simple Axios And Data Fetching :: Edit src/pages/axios.tsx</div>;
 }
 
 // TODO: fetch data with axios to use in app
-// RestApi: axios.get("/api/users") => [{name: "amir"}]
+// RestApi: axios.get("http://localhost:3000/api/users") => [{name: "amir"}]
 // NOTE: data fetching should happen in server side
 // Resource: https://nextjs.org/docs/basic-features/data-fetching
 // Resource: https://github.com/axios/axios
